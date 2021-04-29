48ef48b1e9429504aa75798dfe3a306aab43f6ac
ronyparra <ronyparra@outlook.com>
2021-04-29 16:35:28 -0400

    c

--- src/constans/appSettings.js
+++ src/constans/appSettings.js
@@ -1,4 +1,4 @@
-export const baseURL = 'http://192.168.0.17:8001/';
+//export const baseURL = 'http://192.168.0.17:8001/';
 //export const baseURL = 'http://localhost:8001/';
-//export const baseURL = "https://servertecnico.integral.com.py/"
+export const baseURL = "https://servertecnico.integral.com.py/"
 
