b6813c8039ae4cca0aba3e137ced5666a46a07be
Rony <ronyparra@outlook.com>
2022-08-11 01:57:34 -0400

    Feat change login background color

--- src/layout/login/Login.vue
+++ src/layout/login/Login.vue
@@ -1,7 +1,17 @@
 <template>
   <v-app>
-    <v-main>
+    <v-main class="primary">
       <router-view />
     </v-main>
   </v-app>
 </template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
